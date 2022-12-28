59d354011dd7f41982729434c00bc4d3fcdadf50
astor4577 <121545137+astor4577@users.noreply.github.com>
2022-12-28 17:38:13 +0800

    Update DiscordBot.js

--- DiscordBot.js
+++ DiscordBot.js
@@ -4,16 +4,16 @@ const client = new Discord.Client();
 
 // 連上線時的事件
 client.on('ready', () => {
-    console.log(`已連結到 ${client.user.tag}!`);
+    console.log(`已連線到 ${client.user.tag}!`);
 });
 
 // 當 Bot 接收到訊息時的事件
 client.on('message', msg => {
     // 如果訊息的內容是 'ping'
     if (msg.content === 'ping') {
-        //  機器人則回應 'Pong'
+        // 則 Bot 回應 'Pong'
         msg.reply('pong');
     }
 });
 
-client.login(你的token碼);
+client.login(token);
